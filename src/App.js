ae16018dea40b540682300266d94c18dd458a180
julieca <ju_lieca@yahoo.com>
2020-04-13 17:02:52 +0700

    deleting unnesessary component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import History from './pages/History';
-import Search from './pages/Search' ;
-import Login from './pages/Login';
+import Search from './pages/Search';
 
 
 import './App.css';
@@ -24,12 +20,7 @@ const App = ({ store }) => (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/detail/:id" component={Detail} />
-          <Route path="/history" component={History} />
-          <Route path="/search" component={Search} />
-          <Route path="/login" component={Login} />
-
+          <Route path="/" component={Search} />
         </Switch>
       </Router>
     </Provider>
