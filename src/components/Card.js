97a431d890f385176c578d1ce0300cef4fe44a6b
ju_lieca@yahoo.com <ju_lieca@yahoo.com>
2020-04-14 13:43:03 +0700

    fixing some ui

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import "../assets/css/Card.css"
-import { Card, CardContent, Typography } from '@material-ui/core';
-const CardTemplate = ({ data }) =>
-  <Card>
-    <CardContent>
-      <Typography component="h2">
-        {data.name}
-      </Typography>
-      <Typography className="price bold">
-        {data.price}
-      </Typography>
-      <Typography variant="body2" component="p">
-        {data.description}
-      </Typography>
-      <Typography color="primary">
-        {data.furniture_style.join(", ")}
-      </Typography>
-      <Typography className="time bold" color="primary">
-        {data.delivery_time}
-      </Typography>
-    </CardContent>
-  </Card>
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+const CardTemplate = ({ data }) => {
+  return (
+    <Grid item xs={6}>
 
+      <Card>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={9}>
+              <Typography className="bold">
+                {data.name}
+              </Typography>
+            </Grid>
+            <Grid item xs={3} >
+              <Typography className="price bold">
+                {data.price}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+
+              <Typography variant="body2" component="p">
+
+                {data.description}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+
+
+              <Typography color="primary">
+                {data.furniture_style.join(", ")}
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12}>
+              {/* <Typography className="time bold" color="primary"> */}
+              <Typography className="time bold">
+                Delivery Days {data.delivery_time}.
+              </Typography>
+            </Grid>
+
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+
+
+  );
+}
 export default CardTemplate;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -23,6 +23,7 @@ const Search = props => {
   const [datares, setDatares] = React.useState([]);
   const [furnitureStyle, setfurnitureStyle] = React.useState([]);
   const [deliveryTime, setdeliveryTime] = React.useState([]);
+  const [deliveryDay, setdeliveryDay] = React.useState([]);
 
   useEffect(() => {
     props.getData();
@@ -61,74 +62,115 @@ const Search = props => {
   return (
     //
     <div>
-      <div>
-        <TextField
-          value={keyword}
-          label={keywordLabel}
-          placeholder={keywordLabel}
-          onChange={(input) => setKeyword(input.target.value)}
-        />
-        <FormControl>
+      <div style={{ "margin": "40px" }}>
+        <Grid container
+          spacing={2}
+          justify="center"
+          alignItems="center" >
+          <Grid item xs={6}>
+            <TextField
+              value={keyword}
+              label={keywordLabel}
+              placeholder={keywordLabel}
+              onChange={(input) => setKeyword(input.target.value)}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}>
 
-        <InputLabel id="furniturStyleL">{furnitureSLabel}</InputLabel>
-        <Select
-          multiple
-          labelId="furniturStyleL"
-          value={furnitureStyle}
-          onChange={(e) => { setfurnitureStyle(e.target.value) }}
-          renderValue={(selected) => selected.join(', ')}
-        >
-          {
-            props.furnitureStyles && props.furnitureStyles.map((furniture) => (
-              <MenuItem
-                key={furniture} value={furniture}
+            <FormControl style={{ "width": "100%" }}>
+              <InputLabel id="furniturStyleL">{furnitureSLabel}</InputLabel>
+              <Select
+                multiple
+                labelId="furniturStyleL"
+                value={furnitureStyle}
+                onChange={(e) => { setfurnitureStyle(e.target.value) }}
+                renderValue={(selected) => selected.join(', ')}
               >
-                <ListItemText primary={furniture} />
-                <Checkbox checked={furnitureStyle.indexOf(furniture) > -1} />
-              </MenuItem>
-            ))
-          }
-        </Select>
-        </FormControl>
-        <FormControl>
+                {
+                  props.furnitureStyles && props.furnitureStyles.map((furniture) => (
+                    <MenuItem
+                      key={furniture} value={furniture}
+                    >
+                      <ListItemText primary={furniture} />
+                      <Checkbox checked={furnitureStyle.indexOf(furniture) > -1} />
+                    </MenuItem>
+                  ))
+                }
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={6}>
 
-        <InputLabel id="deliveryTimeL">{deliveryTLabel}</InputLabel>
-        <Select
-          multiple
-          labelId="deliveryTimeL"
-          value={deliveryTime}
-          onChange={(e) => { setdeliveryTime(e.target.value) }}
-          renderValue={(selected) => selected.join(', ')}
-        >
-          {
-            deliveryTChoice.map((delivery) => (
-              <MenuItem
-                key={delivery} value={delivery}
+            <FormControl style={{ "width": "100%" }}>
+              <InputLabel id="deliveryTimeL">{deliveryTLabel}</InputLabel>
+              <Select
+                multiple
+                labelId="deliveryTimeL"
+                value={deliveryTime}
+                onChange={(e) => {
+                  setdeliveryTime(e.target.value)
+
+                  let days = [];
+                  for (let i = 0; i < e.target.value.length; i++) {
+                    var index = deliveryTChoice.indexOf(e.target.value[i]);
+                    let day = 0
+                    switch (index) {
+                      case 0: day = 7; break;
+                      case 1: day = 14; break;
+                      case 2: day = 30; break;
+                      case 3: day = -1; break;
+                    }
+                    days.push(day);
+                  }
+                  setdeliveryDay(days);
+                }}
+                renderValue={(selected) => selected.join(', ')}
               >
-                <ListItemText primary={delivery} />
-                <Checkbox checked={deliveryTime.indexOf(delivery) > -1} />
-              </MenuItem>
-            )
-            )
-          }
-        </Select>
-        </FormControl>
+                {
+                  deliveryTChoice.map((delivery) => (
+                    <MenuItem
+                      key={delivery} value={delivery}
+                    >
+                      <ListItemText primary={delivery} />
+                      <Checkbox checked={deliveryTime.indexOf(delivery) > -1} />
+                    </MenuItem>
+                  )
+                  )
+                }
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+
       </div>
+      <div style={{ "margin": "40px" }}>
 
-      <Grid container justify="center" alignItems="center">
-        <Grid item xs={12} sm={9} md={6}>
+        <Grid container
+          spacing={4}
+          // justify="center" 
+          alignItems="center" >
+          {/* <Grid item xs={12} sm={9} md={6}> */}
 
           {datares && datares.map((h, i) =>
             <Card key={i}
-              id={h.id}
-              imageUrl={h.imageUrl}
-              title={h.title}
-              price={h.price} />
+              data={h}
+            // id={h.id}
+            // name={h.name}
+            // description={h.description}
+            // furniture_style={h.furniture_style}
+            // delivery_time={h.delivery_time}
+            // // imageUrl={h.imageUrl}
+            // // title={h.title}
+            // price={h.price} 
+            />
           )}
 
         </Grid>
-      </Grid>
+      </div>
     </div>
+
   );
 }
 
